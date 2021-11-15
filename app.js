eadb5e7e07a6deb64c26d561f13f844828acd642
dicentes <rlbradshaw77@gmail.com>
2021-11-14 22:03:44 -0500

    fixed non-automove verse selection, also added display of the current verse number

--- app.js
+++ app.js
@@ -27,28 +27,6 @@ app.listen(process.env.port || 3000);
 
 console.log("Running at Port 3000");
 
-//commenting this out because I dont think I need it anymore
-/*
-fs.readFile("test.json", "utf8", (err, jsonString) => {
-  if (err) {
-    console.log("Error reading file:", err);
-    return;
-  }
-  try {
-    const cardinfo = JSON.parse(jsonString);
-    console.log("Random sentence:", cardinfo.Scripture); 
-    //I know now that we can pass the cardinfo.scripture value on 
-    //i assume it can be done dynamincally through rendering but right now
-    //it's melting my brain to actually figure that out 
-    global.scriptureverse = cardinfo.Scripture
-  } catch (err) {
-    console.log("Error parsing JSON string:", err);
-  }
-});
-*/ 
-//EXPERIMENTAL JSON READING GOING ON BELOW VERY SCARY 
-//now we're going to try to replicate the function above but with user input
-//it should read through the json arrray and match it up with the appropriate verse
 function BibleReader (){
 
   if(typeof verseposition === 'undefined'){
@@ -78,16 +56,30 @@ function BibleReader (){
         console.log("Error reading file:", err);
         return;
       }
-      try {
+      try { //if verseposition is already defined (in the instance that the user selects it or previous verses have been done)
         const verseblock = JSON.parse(jsonString);
         //take a user provided chapter and verse number and then log it into the console 
         const userchapter = 0;
-        global.currentverse = verseblock.chapters[userchapter].verses[verseposition].text
-        // now i want to simply print out how many chapters there are and how many verses there are. 
-        //find the nth position and then print out the number? like if there are 5 chapters, it should go to the last elemetn and then print the number? 
-        global.booktitle = verseblock.book;
-        global.chaptercount = Object.keys(verseblock.chapters).length; //this kind of works but it starts from the 0th 
-        global.versecount = Object.keys(verseblock.chapters[0].verses).length; //this works but it starts from the 0th
+        console.log("Is automove turned on? " + automove);
+        //writing an if/else statement for whether or not the user has "automove" turned on 
+        if (automove === "yes"){ //the entire automove functionality is broken because i need to figure out how to do a counter 
+          global.verseposition = parseInt(verseposition) + 1; 
+          console.log("automove is turned on, so we're going to the next verse");
+          global.currentverse = verseblock.chapters[userchapter].verses[verseposition].text
+          // now i want to simply print out how many chapters there are and how many verses there are. 
+          //find the nth position and then print out the number? like if there are 5 chapters, it should go to the last elemetn and then print the number? 
+          global.booktitle = verseblock.book;
+          global.chaptercount = Object.keys(verseblock.chapters).length; //this kind of works but it starts from the 0th 
+          global.versecount = Object.keys(verseblock.chapters[0].verses).length; //this works but it starts from the 0th
+        } else { //the non-automove functionality is working fine. 
+          console.log("Automove is turned off, so we're showing the user selected verse ");
+          global.currentverse = verseblock.chapters[userchapter].verses[verseposition].text
+          // now i want to simply print out how many chapters there are and how many verses there are. 
+          //find the nth position and then print out the number? like if there are 5 chapters, it should go to the last elemetn and then print the number? 
+          global.booktitle = verseblock.book;
+          global.chaptercount = Object.keys(verseblock.chapters).length; //this kind of works but it starts from the 0th 
+          global.versecount = Object.keys(verseblock.chapters[0].verses).length; //this works but it starts from the 0th
+        }
       } catch (err) {
         console.log("Error parsing JSON string:", err);
       }
@@ -99,7 +91,8 @@ BibleReader();
 
 app.post('/', (req, res) =>{ 
   global.verseposition = req.body.verse1; //this grabs the right string info we want
-  console.log(verseposition);
+  global.automove = req.body.automove; //this determines if the user wanted to automatically progress 
+  console.log("verseposition received from the user is: " + verseposition);
   BibleReader();
   res.sendStatus(200); //tell the server we've got it
 });
