dbbf3ebf0539b2b40f2bccdbe05ba4c70883ba88
dicentes <rlbradshaw77@gmail.com>
2021-11-16 20:13:12 -0500

    fixed several things included the forwarding through verses on automove

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function BibleReader (){
         const verseblock = JSON.parse(jsonString);
         //take a user provided chapter and verse number and then log it into the console 
         const userchapter = 0;
-        const verseposition = 0;
+        global.verseposition = 0;
         global.currentverse = verseblock.chapters[userchapter].verses[verseposition].text
         // now i want to simply print out how many chapters there are and how many verses there are. 
         //find the nth position and then print out the number? like if there are 5 chapters, it should go to the last elemetn and then print the number? 
@@ -63,15 +63,28 @@ function BibleReader (){
         console.log("Is automove turned on? " + automove);
         //writing an if/else statement for whether or not the user has "automove" turned on 
         if (automove === "yes"){ //the entire automove functionality is broken because i need to figure out how to do a counter 
-          global.verseposition = parseInt(verseposition) + 1; 
-          console.log("automove is turned on, so we're going to the next verse");
-          global.currentverse = verseblock.chapters[userchapter].verses[verseposition].text
+          global.verseposition = verseposition;
+          if (verseposition >= Object.keys(verseblock.chapters[0].verses).length){
+          console.log("user has submitted a verse that doesn't exist, start from the beginning ");
+          global.verseposition = 0;
+          console.log(verseposition);
+          currentverse = verseblock.chapters[userchapter].verses[verseposition].text
           // now i want to simply print out how many chapters there are and how many verses there are. 
           //find the nth position and then print out the number? like if there are 5 chapters, it should go to the last elemetn and then print the number? 
           global.booktitle = verseblock.book;
           global.chaptercount = Object.keys(verseblock.chapters).length; //this kind of works but it starts from the 0th 
           global.versecount = Object.keys(verseblock.chapters[0].verses).length; //this works but it starts from the 0th
-        } else { //the non-automove functionality is working fine. 
+          } else {
+            console.log("user verse selection exists, continue on");
+            console.log("automove is turned on, so we're going to the next verse");
+            global.currentverse = verseblock.chapters[userchapter].verses[verseposition].text
+            // now i want to simply print out how many chapters there are and how many verses there are. 
+            //find the nth position and then print out the number? like if there are 5 chapters, it should go to the last elemetn and then print the number? 
+            global.booktitle = verseblock.book;
+            global.chaptercount = Object.keys(verseblock.chapters).length; //this kind of works but it starts from the 0th 
+            global.versecount = Object.keys(verseblock.chapters[0].verses).length; //this works but it starts from the 0th
+          }
+        } else { //NON-AUTOMOVE SECTION 
           console.log("Automove is turned off, so we're showing the user selected verse ");
           global.currentverse = verseblock.chapters[userchapter].verses[verseposition].text
           // now i want to simply print out how many chapters there are and how many verses there are. 
