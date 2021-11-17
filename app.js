571dedffd725272d459f89c65efb7d061617e929
dicentes <rlbradshaw77@gmail.com>
2021-11-16 23:16:32 -0500

    second try at committing all changes for improvement

--- app.js
+++ app.js
@@ -1,31 +1,100 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require("express");
 const fs = require("fs");
 const app = express();
+const bcrypt = require("bcrypt")
 const path = require("path");
 const router = express.Router();
 const qs = require('querystring');
-app.use(express.urlencoded({ extended: true }));
-// this is all new stuff could delete
+const passport = require('passport');
+const { request } = require("http");
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override') 
+app.use(express.urlencoded({ extended: true })); 
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
-//end of experimental code 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-router.get("/", (req, res) => {
-  res.render("index", {user: "Rob"})
-});
 
-router.get("/about", (req, res) => {
-  res.render("about", { title: "Hey", message: "Hello there!" });
-});
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
+
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index', { name: req.user.name })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+    console.log("User successfully registered.")
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
 
 app.use("/", router);
 app.listen(process.env.port || 3000);
-//this is the POST request from the client if someone selects a new verse
-
-
-
-console.log("Running at Port 3000");
 
 function BibleReader (){
 
